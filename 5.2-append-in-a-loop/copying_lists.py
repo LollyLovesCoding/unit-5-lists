c7a7ac08c450fdd7e3b51bddf4d7c552b07eeef4
Lilyana Zhang <impro4everrr@gmail.com>
2025-11-05 10:30:53 -0500

    Create copying_lists.py

---
+++ 5.2-append-in-a-loop/copying_lists.py
@@ -0,0 +1,21 @@
+from random import randrange
+
+list1 = []
+list2 = []
+
+for n in range(10):
+    list1.append(randrange(10, 101))
+
+for i in range(len(list1)):
+    num = list1[i]
+    list2.append(num)
+
+list1[9] = -7
+
+print("List 1: ", end="")
+for i in range(len(list1)):
+    print(list1[i], end=" ")
+
+print("\nList 2: ", end="")
+for i in range(len(list2)):
+    print(list2[i], end=" ")
